f0f9488f31ec666c7ad5440da376d5cfdc89e82c
TuckoxXL <sebastianbelluccini@gmail.com>
2023-09-23 19:20:27 -0300

    Cañon
    se agrego la lógica de disparo del cañón, se agrego los slider para poder modificar la rotación, fuerza y peso del cañón

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject bullet;
+    public Transform spawnbullet;
+
+    public float shootForce = 1500;
+    public float shootRate = 0.5f;
+    private float shootRatetime = 0;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time > shootRatetime)
+            {
+                GameObject newbullet;
+
+                newbullet = Instantiate(bullet, spawnbullet.position, spawnbullet.rotation);
+
+                newbullet.GetComponent<Rigidbody>().AddForce(spawnbullet.forward * shootForce);
+
+                shootRatetime = Time.time + shootRate;
+
+                Destroy(newbullet, 3);
+
+            }
+        }
     }
 }
